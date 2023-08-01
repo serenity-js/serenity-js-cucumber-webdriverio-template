91972e76a13fb3eb79d44b5a44244126e9bdef8b
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-08-01 16:48:40 +0100

    Code clean-up

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,7 +1,3 @@
-import { ConsoleReporter } from '@serenity-js/console-reporter';
-import { ArtifactArchiver } from '@serenity-js/core';
-import { SerenityBDDReporter } from '@serenity-js/serenity-bdd';
-import { Photographer, TakePhotosOfInteractions } from '@serenity-js/web';
 import { WebdriverIOConfig } from '@serenity-js/webdriverio';
 
 export const config: WebdriverIOConfig = {
